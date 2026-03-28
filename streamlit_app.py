95d1339e4b9d5d27c2b2102fcdf1ba4dea602b73
sumits20 <sumitsardaruk@gmail.com>
2026-03-28 12:42:58 +0000

    Add Streamlit app to load recent WordPress posts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit as st
+from tools.wordpress_tools import get_recent_posts
 
+st.title("Riolabs Content Agent")
+
+if st.button("Load recent posts"):
+    posts = get_recent_posts(5)
+    st.write(posts)
